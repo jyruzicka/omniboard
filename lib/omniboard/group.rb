6c2418a80dd2073076d3fe3b5dc362c468b225c3
Jan-Yves Ruzicka <jan@1klb.com>
2016-05-23 14:20:23 +0100

    Add Group.default_colour

--- lib/omniboard/group.rb
+++ lib/omniboard/group.rb
@@ -1,36 +1,57 @@
+# This class represents a "group" of Projects.
 class Omniboard::Group
 	include Comparable
 
+	# The name of the group
 	attr_accessor :name
+
+	# The colour representation of a group
 	attr_accessor :colour
 
+	# Array of all groups in the board
 	@groups = []
 	
 	class << self
+		# Global values for group colour brightness and saturation
 		attr_accessor :brightness, :saturation
 
+		# Find a group by name
 		def [] str
-			raise(ArgumentError, "Must search by string") if !str.is_a?(String)
+			raise(ArgumentError, "Group[] called with non-string (#{str.klass}) argument: you must search groups by string.") if
+				!str.is_a?(String)
+			
 			@groups.find{ |g| g.name == str } || new(str)
 		end
 
+		# Returns the index of a group in the group array
 		def index(g)
 			@groups.index(g)
 		end
 
+		# How many groups do we have in our array?
 		def size
 			@groups.size
 		end
 
+		# Add a new group to the groups array. Also resets all colour assignments for the group.
+		# Note: usually called from initializer.
 		def add(g)
 			@groups.each(&:reset_colour)
 			@groups << g
 		end
 
+		# If we don't have any groups on our board, this is our default
+		# colour for ungrouped projects
 		def default_colour
 			Omniboard::Colour.new(0).standard
 		end
 		alias_method :colour, :default_colour
+
+		# If we don't have any groups on our board, this is our default
+		# light colour for ungrouped projects
+		def light_colour
+			Omniboard::Colour.neW(0).light
+		end
 	end
 
 	def initialize(name)
