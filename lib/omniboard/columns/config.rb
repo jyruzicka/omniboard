817c1b778f2fcdc1f5643e70b41d4de1f74af758
Jan-Yves Ruzicka <jan@1klb.com>
2016-09-10 11:37:56 +1200

    Added OmniFocus sort as default to Config

--- lib/omniboard/columns/config.rb
+++ lib/omniboard/columns/config.rb
@@ -3,8 +3,37 @@ Omniboard::Column.config do
 	# Group projects by their containing folders
 	group_by{ |p| p.container || "" }
 
-	# Sort by rank, with top level projects at the top
-	sort_groups{ |c| c.nil? ? 0 : c.rank }
+	# This somewhat-complex code sorts projects by their order within OmniFocus
+  # Want to help? Find a more elegant way to write this :P
+  sort_groups do |a,b|
+    if a == ""
+      -1
+    elsif b == ""
+      1
+    else
+      # Compare from outside in
+      a_ancestry = [a] + a.ancestry
+      b_ancestry = [b] + b.ancestry
+      
+      a_cursor = a_ancestry.size - 1
+      b_cursor = b_ancestry.size - 1
+
+      sort_value = 0
+      until a_cursor < 0 || b_cursor < 0 || sort_value != 0
+        sort_value = (a_ancestry[a_cursor].rank <=> b_ancestry[b_cursor].rank)
+        if sort_value == 0
+          a_cursor -= 1
+          b_cursor -= 1
+        end
+      end
+
+      if sort_value != 0
+        sort_value
+      else
+        a_ancestry.size <=> b_ancestry.size
+       end
+    end
+  end
 
 	# Name based on ancestry
 	group_name{ |c| c.nil? ? "Top level" : c.ancestry.map(&:name).reverse.join("→")}
